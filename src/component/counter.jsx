3c8760539330df457e74a68bddaaff265fbabfa8
shoaib <rahmanshoaibn@gmail.com>
2021-06-24 00:50:21 +0530

    added increment property

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-export default class Counter  extends Component {
-    
-    state = {
-        count: 0
-    };
-    
-    render() { 
-        return (<React.Fragment>
-            <span className={this.getCounterFormat()}>{this.formatCounter()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-        </React.Fragment> );
-    }
+export default class Counter extends Component {
+  state = {
+    count: 0
+  };
 
-    getCounterFormat() {
-        let classes = "badge m-3 bg-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
+  handleIncrement = () => {
+    this.setState({count: this.state.count + 1})
+  }
 
-    formatCounter(){
-        const {count} = this.state;
-        return count === 0 ? "zero" : count;
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getCounterFormat()}>{this.formatCounter()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  getCounterFormat() {
+    let classes = "badge m-3 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCounter() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
+  }
 }
